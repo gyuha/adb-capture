caa52a94835bb3ec5332ca15934b8f9c3c78a54b
gyuha <nicegyuha@gmail.com>
2021-05-02 02:28:31 +0900

    대충 UI 구성 하는 중

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
 import sys
 
-from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QWidget, QDial, QSlider, QApplication, QBoxLayout
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
 
 
-class Form(QWidget):
+form_class = uic.loadUiType("main.ui")[0]
+
+
+class MainWindow(QMainWindow, form_class):
     def __init__(self):
-        QWidget.__init__(self, flags=Qt.Widget)
-        self.dl = QDial()
-        self.sd = QSlider(Qt.Horizontal)
-        self.init_widget()
+        super().__init__()
+        self.setupUi(self)
 
-    def init_widget(self):
-        self.setWindowTitle("Signal Slot")
-        form_lbx = QBoxLayout(QBoxLayout.TopToBottom, parent=self)
-        self.setLayout(form_lbx)
+        self.btnPathSelect.clicked.connect(self.onSelectPath)
+        self.btnStart.clicked.connect(self.onStart)
+        self.btnStop.clicked.connect(self.onStop)
 
-        form_lbx.addWidget(self.dl)
-        form_lbx.addWidget(self.sd)
+    def onSelectPath(self):
+        print("select path Clicked")
 
-        # 시그널 슬롯 연결
-        # 다이얼의 값이 변하면 슬라이더의 값을 변경하는 슬롯과 연결
-        # 슬라이더의 값이 변화하면 다이얼의 값을 변경하는 슬롯과 연결
-        # 두 위젯의 valueChange 시그널은 현재값을 int형으로 반환
-        # 두 위젯의 setValue 슬롯은 int형만을 받는다.
-        self.dl.valueChanged.connect(self.sd.setValue)
-        self.sd.valueChanged.connect(self.dl.setValue)
+    def onStop(self):
+        print("stop Clicked")
+
+    def onStart(self):
+        print("start Clicked")
+        self.edtCapturePath.setText('start')
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    form = Form()
-    form.show()
+    mainWindow = MainWindow()
+    mainWindow.show()
     exit(app.exec_())
